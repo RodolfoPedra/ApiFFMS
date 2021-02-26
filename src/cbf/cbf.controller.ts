fcd88bdf32508bd846290997dc0954df0b3d964e
Rodolfo Pedra <rodolfaldrovandip@gmail.com>
2021-02-26 01:38:07 -0400

    inclusao de endpoint personalizado campeonato com exercicio

--- src/cbf/cbf.controller.ts
+++ src/cbf/cbf.controller.ts
@@ -1,9 +1,48 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import Axios from 'axios';
 import { CbfService } from './shared/cbf.service';
+import {authDados} from '../utils/authCBF';
+
+
+const sub17 =  [{
+  "codigo_campeonato": 431,
+  "CodigoFederacao": 5,
+  "Campeonato_Nome": "Sul-Mato-Grossense Sub 17\t",
+  "Ordem": 4,
+  "exercicio": 2020,
+  "Codigo_Fase": 135
+}];
 
 @Controller('cbf')
 export class CbfController {
   constructor(private CbfService: CbfService) {}
+
+  @Get('CampeonatosExercicio/:exercicio')
+  async campeonatos(@Param('exercicio') exercicio: number) {
+    const _campeonatoAno = await Axios.get(`${authDados.baseUrl}CampeonatosFases`, {
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Authorization: `Basic ${authDados.auth}`,
+      },
+    }).then((json) => json.data);
+
+    const _campeonato = await Axios.get(`${authDados.baseUrl}Campeonatos`, {
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Authorization: `Basic ${authDados.auth}`,
+      },
+    }).then((json) => json.data);
+
+    const _newCampeonatoAno = _campeonatoAno.reduce((acc, item, index) => {
+        const newCampeonato = _campeonato.find((v) => v.codigo_campeonato == item.codigo_campeonato)
+        const ca = Object.assign(item, {Campeonato_Nome: newCampeonato.Campeonato_Nome});
+        return [...acc, (acc[index] = ca)];
+    },[])
+
+    const newCampeonato = _newCampeonatoAno.concat(sub17).filter((v) => v.Codigo_Fase == 135 && v.exercicio == exercicio);
+    
+    return newCampeonato;
+  }
 
   @Get('AlteracoesJogos')
   getAlteracoesJogos(): any {
