55b7c1ddef534ce71132ffedf528efa520d66331
RodolfoPedra <40034261+RodolfoPedra@users.noreply.github.com>
2021-04-21 21:55:09 -0400

    Delete src/utils directory

--- src/utils/authCBF.ts
+++
@@ -1,7 +0,0 @@
-const authDados = {
-  baseUrl: 'http://servicos-fdrs.cbf.com.br/api/Schemafdrs/',
-  auth:
-    'bXMucmVnaXN0cm9AY2JmLmNvbS5icjpGbWF0b2dyb3Nzb2Rvc3VsIzIwMTUxMTEwIw==',
-};
-
-export { authDados };
